eb2732f12bbcff62a12d569140063e40b32fd05a
MultiTime <64415590+MilanVos@users.noreply.github.com>
2025-06-12 23:29:09 +0200

    /,.

--- src/main/java/nl/multitime/craftKit/CraftKit.java
+++ src/main/java/nl/multitime/craftKit/CraftKit.java
@@ -1,17 +1,45 @@
 package nl.multitime.craftKit;
 
+import nl.multitime.craftKit.commands.KitCommand;
+import nl.multitime.craftKit.commands.StatsCommand;
+import nl.multitime.craftKit.database.DatabaseManager;
+import nl.multitime.craftKit.listeners.PlayerDeathListener;
+import nl.multitime.craftKit.managers.KitManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CraftKit extends JavaPlugin {
 
+    private DatabaseManager databaseManager;
+    private KitManager kitManager;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        this.databaseManager = new DatabaseManager(this);
+        this.databaseManager.initialize();
 
+        this.kitManager = new KitManager();
+
+        getCommand("kit").setExecutor(new KitCommand(this));
+        getCommand("kitstats").setExecutor(new StatsCommand(this));
+
+        getServer().getPluginManager().registerEvents(new PlayerDeathListener(this), this);
+
+        getLogger().info("CraftKit has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (databaseManager != null) {
+            databaseManager.close();
+        }
+        getLogger().info("CraftKit has been disabled!");
+    }
+
+    public DatabaseManager getDatabaseManager() {
+        return databaseManager;
+    }
+
+    public KitManager getKitManager() {
+        return kitManager;
     }
 }
